bb9db1787b5e746d1207821b7824a2e7f4661abe
KevinNguyenSr <kevinian@csu.fullerton.edu>
2024-12-03 18:22:15 -0800

    Update authenticate.middleware.js

--- backend/src/middleware/authenticate.middleware.js
+++ backend/src/middleware/authenticate.middleware.js
@@ -1,45 +1,28 @@
 import getCookie from '../utils/cookie.js';
 import { verifyToken } from '../utils/jwt.js';
 
-const authenticate = async (req, res, next) => {
-  try {
-    const cookies = getCookie(req);
+const validateUser = async (req, res, next) => {
+  const cookies = getCookie(req);
 
-    if (cookies.length === 0) {
-      return res.status(401).json({
-        message: 'You are not authorized to access this resource.',
-        isAuthenticated: false,
-      });
-    }
-
-    const fullToken = cookies.find((cookie) => cookie.startsWith('postagramToken'));
-
-    if (!fullToken) {
-      return res.status(401).json({
-        message: 'You are not authorized to access this resource.',
-        isAuthenticated: false,
-      });
-    }
-
-    const token = fullToken.split('=')[1];
-
-    const verified = verifyToken({ token });
-    if (!verified) {
-      return res.status(401).json({
-        message: 'Token expired, please login again',
-        isAuthenticated: false,
-      });
-    }
-
-    req.tokenData = verified;
-    next();
-  } catch (err) {
-    console.error('Error in authentication middleware:', err.message);
-    return res.status(500).json({
-      message: 'An internal server error occurred.',
-      isAuthenticated: false,
-    });
+  if(cookies.length === 0) {
+    return res.status(401).json({ message: 'You are not authorized to access this resource.', isAuthenticated: false });
   }
+
+  const fullToken = cookies.find((cookie) => cookie.startsWith('postagramToken'));
+
+  if (!fullToken) {
+    return res.status(401).json({ message: 'You are not authorized to access this resource.', isAuthenticated: false });
+  }
+
+  const token = fullToken.split('=')[1];
+
+  const verified = verifyToken({ token });
+
+  if (verified === undefined) {
+    return res.status(401).json({ message: 'Token expired, please login again', isAuthenticated: false });
+  }
+
+  return next();
 };
 
-export default authenticate;
+export default validateUser;
