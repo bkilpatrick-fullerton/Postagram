29186d028b76459d247b2063ff04f585321d3b1d
Kourosh Alasti <coding@kouroshalasti.com>
2024-12-16 21:07:24 -0800

    Update cookie.js

--- backend/src/utils/cookie.js
+++ backend/src/utils/cookie.js
@@ -1,6 +1,9 @@
+import logger from "./logger";
+
 const getCookie = (req) => {
   // grab all cookies
   const { cookie } = req.headers;
+  logger.debug(cookie);
 
   // if no cookies []
   if (!cookie) return [];
